2ac9903ccd99576a7b78686f6af1bcf0ee83f178
Jon Sanders <jsanders@oeb.harvard.edu>
2016-09-28 11:53:44 -0400

    Initial creation of blank kneaddata scripts

---
+++ qp_shotgun/kneaddata/tests/test_kneaddata.py
@@ -0,0 +1,62 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2014--, The Qiita Development Team.
+#
+# Distributed under the terms of the BSD 3-clause License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# -----------------------------------------------------------------------------
+
+from unittest import TestCase, main
+from os import close, environ
+from tempfile import mkstemp
+from json import dumps
+
+from qiita_client import QiitaClient
+
+from qp_shotgun.humann2.humann2 import (
+    get_sample_names_by_run_prefix)
+
+from qp_shotgun.kneaddata.kneaddata import (
+    )
+
+
+CLIENT_ID = '19ndkO3oMKsoChjVVWluF7QkxHRfYhTKSFbAVt8IhK7gZgDaO4'
+CLIENT_SECRET = ('J7FfQ7CQdOxuKhQAf1eoGgBAE81Ns8Gu3EKaWFm3IO2JKh'
+                 'AmmCWZuabe0O5Mp28s1')
+
+
+class KneaddataTests(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        server_cert = environ.get('QIITA_SERVER_CERT', None)
+        cls.qclient = QiitaClient("https://localhost:21174", CLIENT_ID,
+                                  CLIENT_SECRET, server_cert=server_cert)
+        cls.params = {}
+        cls._clean_up_files = []
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.qclient.post('/apitest/reset/')
+
+
+
+MAPPING_FILE = (
+    "#SampleID\tplatform\tbarcode\texperiment_design_description\t"
+    "library_construction_protocol\tcenter_name\tprimer\trun_prefix\t"
+    "instrument_model\tDescription\n"
+    "SKB7.640196\tILLUMINA\tA\tA\tA\tANL\tA\ts3\tIllumina MiSeq\tdesc1\n"
+    "SKB8.640193\tILLUMINA\tA\tA\tA\tANL\tA\ts1\tIllumina MiSeq\tdesc2\n"
+    "SKD8.640184\tILLUMINA\tA\tA\tA\tANL\tA\ts2\tIllumina MiSeq\tdesc3\n"
+)
+
+MAPPING_FILE_2 = (
+    "#SampleID\tplatform\tbarcode\texperiment_design_description\t"
+    "library_construction_protocol\tcenter_name\tprimer\t"
+    "run_prefix\tinstrument_model\tDescription\n"
+    "SKB7.640196\tILLUMINA\tA\tA\tA\tANL\tA\ts3\tIllumina MiSeq\tdesc1\n"
+    "SKB8.640193\tILLUMINA\tA\tA\tA\tANL\tA\ts1\tIllumina MiSeq\tdesc2\n"
+    "SKD8.640184\tILLUMINA\tA\tA\tA\tANL\tA\ts1\tIllumina MiSeq\tdesc3\n"
+)
+
+if __name__ == '__main__':
+    main()
