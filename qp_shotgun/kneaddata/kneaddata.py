b142aa8dfa7d09fdab61c98267f8f9d9d9d76ac9
Jon Sanders <jsanders@oeb.harvard.edu>
2016-09-29 12:10:29 -0400

    Created empty kneaddata command

--- qp_shotgun/kneaddata/kneaddata.py
+++ qp_shotgun/kneaddata/kneaddata.py
@@ -12,3 +12,48 @@ from future.utils import viewitems
 import pandas as pd
 
 
+
+def kneaddata(qclient, job_id, parameters, out_dir):
+    """Run kneaddata with the given parameters
+
+    Parameters
+    ----------
+    qclient : tgp.qiita_client.QiitaClient
+        The Qiita server client
+    job_id : str
+        The job id
+    parameters : dict
+        The parameter values to run split libraries
+    out_dir : str
+        Yhe path to the job's output directory
+
+    Returns
+    -------
+    bool, list, str
+        The results of the job
+    """
+    # Step 1 get the rest of the information need to run kneaddata
+    qclient.update_job_step(job_id, "Step 1 of 3: Collecting information")
+    artifact_id = parameters['input_data']
+
+    # Get the artifact filepath information
+    artifact_info = qclient.get("/qiita_db/artifacts/%s/" % artifact_id)
+    fps = artifact_info['files']
+
+    # Get the artifact type
+    artifact_type = artifact_info['type']
+
+    # Get the artifact metadata
+    prep_info = qclient.get('/qiita_db/prep_template/%s/'
+                            % artifact_info['prep_information'][0])
+    qiime_map = prep_info['qiime-map']
+
+    # Step 2 generating command humann2
+    qclient.update_job_step(job_id, "Step 2 of 3: Generating kneaddata command")
+
+    # Step 3 execute humann2: TODO
+    qclient.update_job_step(job_id, "Step 3 of 3: Executing kneaddata")
+
+    artifacts_info = []
+
+    return True, artifacts_info, ""
