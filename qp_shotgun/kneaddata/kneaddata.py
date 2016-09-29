8b5abb39d97f786c0d5d4cdb4ac960c45649af55
Jon Sanders <jsanders@oeb.harvard.edu>
2016-09-29 19:53:10 -0400

    Added method for matching reads and run prefixes

--- qp_shotgun/kneaddata/kneaddata.py
+++ qp_shotgun/kneaddata/kneaddata.py
@@ -11,11 +11,93 @@ from os.path import basename, join
 from future.utils import viewitems
 import pandas as pd
 
-from .humann2.humann2 import get_sample_names_by_run_prefix
+from qiita_client.util import system_call, get_sample_names_by_run_prefix
 
-def generate_humann2_analysis_commands(forward_seqs, reverse_seqs, map_file,
+def make_read_pairs_per_sample(forward_seqs, reverse_seqs, map_file):
+    """Recovers read pairing information
+
+    Parameters
+    ----------
+    forward_seqs : list of str
+        The list of forward seqs filepaths
+    reverse_seqs : list of str
+        The list of reverse seqs filepaths
+    map_file : str
+        The path to the mapping file
+
+    Returns
+    -------
+    samples: list of tup
+        list of 3-tuples with run prefix, sample name, fwd read fp, rev read fp
+
+    Raises
+    ------
+    ValueError
+        If the rev is not an empty list and the same length than fwd seqs
+        The prefixes of the run_prefix don't match the file names
+
+    Notes
+    -----
+    """
+
+    # sort forward seqs
+    forward_seqs.sort()
+
+    # check that rev seqs are same len
+    if reverse_seqs:
+        if len(forward_seqs) != len(reverse_seqs):
+            raise ValueError('Your reverse and forward files are of different '
+                             'length. Forward: %s. Reverse: %s.' %
+                             (', '.join(forward_seqs),
+                              ', '.join(reverse_seqs)))
+        reverse_seqs.sort()
+
+    # get run prefixes
+    # These are prefixes that should match uniquely to forward reads
+    # sn_by_rp is dict of samples keyed by run prefixes
+    sn_by_rp = get_sample_names_by_run_prefix(map_file)
+
+    # make pairings
+    samples = []
+    for i, f_fp in enumerate(forward_seqs):
+        # f_fp is the fwd read filepath
+        f_fn = basename(f_fp)
+
+        # iterate over run prefixes and make sure only one matches
+        run_prefix = None
+        for rp in sn_by_rp:
+            if f_fn.startswith(rp) and run_prefix is None:
+                run_prefix = rp
+            elif f_fn.startswith(rp) and run_prefix is not None:
+                raise ValueError('Multiple forward reads match this run prefix:'
+                                 ' %s' % rp)
+        
+        # make sure that we got one matching run prefix:
+        if run_prefix is None:
+            raise ValueError('No run prefix matching this fwd read: %s'
+                             % f_fn)
+
+        # if we have reverse reads, make sure the matching pair also
+        # matches the run prefix:
+        if reverse_seqs and not reverse_seqs[i].startswith(run_prefix):
+            raise ValueError('Reverse read does not match this run prefix. '
+                             'Run prefix: %s\nForward read: %s\n'
+                             'Reverse read: %s\n' % (run_prefix, f_fn, 
+                                                     basename(reverse_seqs[i])))
+
+        # create the tuple for this read set
+        if reverse_seqs:
+            samples.append((run_prefix, sn_by_rp[run_prefix], f_fp, 
+                            reverse_seqs[i]))
+        else:
+            samples.append((run_prefix, sn_by_rp[run_prefix], f_fp, None))
+
+    return(samples)
+
+
+def generate_kneaddata_analysis_commands(forward_seqs, reverse_seqs, map_file,
                                        out_dir, parameters):
-        """Generates the KneadData commands
+    """Generates the KneadData commands
 
     Parameters
     ----------
