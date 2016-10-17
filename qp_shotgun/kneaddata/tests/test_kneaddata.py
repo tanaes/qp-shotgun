482703da9877ac97279e24eec765938fc6dc0dc2
Jon Sanders <jsanders@oeb.harvard.edu>
2016-10-16 22:06:20 -0700

    Adding tests for kneaddata functionality

--- qp_shotgun/kneaddata/tests/test_kneaddata.py
+++ qp_shotgun/kneaddata/tests/test_kneaddata.py
@@ -8,16 +8,18 @@
 
 from unittest import main
 from os import close, remove
-from os.path import exists, isdir
-from shutil import rmtree
-from tempfile import mkstemp
+from os.path import exists, isdir, join
+from shutil import rmtree, copyfile
+from tempfile import mkstemp, mkdtemp
+from json import dumps
 
 from qiita_client.testing import PluginTestCase
 
 from qp_shotgun.kneaddata import plugin
 from qp_shotgun.kneaddata.kneaddata import (make_read_pairs_per_sample,
                                             generate_kneaddata_commands,
-                                            format_kneaddata_params)
+                                            format_kneaddata_params,
+                                            kneaddata)
 
 
 class KneaddataTests(PluginTestCase):
@@ -53,6 +55,9 @@ class KneaddataTests(PluginTestCase):
                'LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36"')
 
         self.assertEqual(obs, exp)
+
+    def test_break(self):
+        self.assertEqual(1,3)
 
     def test_make_read_pairs_per_sample_match_fwd_rev(self):
         fd, fp = mkstemp()
@@ -171,14 +176,14 @@ class KneaddataTests(PluginTestCase):
         with self.assertRaises(ValueError):
             make_read_pairs_per_sample(fwd_fp, rev_fp, fp)
 
-    def test_generate_humann2_analysis_commands_only_fwd(self):
+    def test_generate_kneaddata_analysis_commands_only_fwd(self):
         fd, fp = mkstemp()
         close(fd)
         with open(fp, 'w') as f:
             f.write(MAPPING_FILE)
         self._clean_up_files.append(fp)
 
-        exp = [
+        exp_cmd = [
             'kneaddata --input "fastq/s1.fastq" '
             '--output "output/s1" --output-prefix "s1" --bowtie2-options '
             '"--very-sensitive" --log-level DEBUG --max-memory 500 '
@@ -203,21 +208,23 @@ class KneaddataTests(PluginTestCase):
             '--trimmomatic-options "ILLUMINACLIP:$trimmomatic/adapters/'
             'TruSeq3-PE-2.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 '
             'MINLEN:36"']
+        exp_pfx = ['s1','s2','s3']
 
-        obs = generate_kneaddata_commands(
+        obs_cmd, obs_pfx = generate_kneaddata_commands(
             ['fastq/s1.fastq', 'fastq/s2.fastq.gz', 'fastq/s3.fastq'], [],
             fp, 'output', self.params)
 
-        self.assertEqual(obs, exp)
+        self.assertEqual(obs_cmd, exp_cmd)
+        self.assertEqual(obs_pfx, exp_pfx)
 
-    def test_generate_humann2_analysis_commands_forward_reverse(self):
+    def test_generate_kneaddata_analysis_commands_forward_reverse(self):
         fd, fp = mkstemp()
         close(fd)
         with open(fp, 'w') as f:
             f.write(MAPPING_FILE)
         self._clean_up_files.append(fp)
 
-        exp = [
+        exp_cmd = [
             'kneaddata --input "fastq/s1.fastq" --input "fastq/s1.R2.fastq" '
             '--output "output/s1" --output-prefix "s1" --bowtie2-options '
             '"--very-sensitive" --log-level DEBUG --max-memory 500 '
@@ -242,12 +249,84 @@ class KneaddataTests(PluginTestCase):
             '--trimmomatic-options "ILLUMINACLIP:$trimmomatic/adapters/'
             'TruSeq3-PE-2.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 '
             'MINLEN:36"']
+        exp_pfx = ['s1','s2','s3']
 
-        obs = generate_kneaddata_commands(
+        obs_cmd, obs_pfx = generate_kneaddata_commands(
             ['fastq/s1.fastq', 'fastq/s2.fastq.gz', 'fastq/s3.fastq'],
             ['fastq/s1.R2.fastq', 'fastq/s2.R2.fastq.gz', 'fastq/s3.R2.fastq'],
             fp, 'output', self.params)
-        self.assertEqual(obs, exp)
+
+        self.assertEqual(obs_cmd, exp_cmd)
+        self.assertEqual(obs_pfx, exp_pfx)
+
+
+    def test_kneaddata(self):
+        # generating filepaths
+        in_dir = mkdtemp()
+        self._clean_up_files.append(in_dir)
+
+        fp1_1 = join(in_dir,'kd_test_1_R1.fastq.gz')
+        fp1_2 = join(in_dir,'kd_test_1_R2.fastq.gz')
+        copyfile('support_files/kd_test_1_R1.fastq.gz', fp1_1)
+        copyfile('support_files/kd_test_1_R2.fastq.gz', fp1_2)
+
+        # inserting new prep template
+        prep_info_dict = {
+            'SKB7.640196': {
+                'run_prefix': 'kd_test_1_'}
+        }
+        data = {'prep_info': dumps(prep_info_dict),
+                # magic #1 = testing study
+                'study': 1,
+                'data_type': 'Metagenomic'}
+        pid = self.qclient.post('/apitest/prep_template/', data=data)['prep']
+
+        # inserting artifacts
+        data = {
+            'filepaths': dumps([
+                (fp1_1, 'raw_forward_seqs'),
+                (fp1_2, 'raw_reverse_seqs')]),
+            'type': "per_sample_FASTQ",
+            'name': "New test artifact",
+            'prep': pid}
+
+        aid = self.qclient.post('/apitest/artifact/', data=data)['artifact']
+
+        self.params['input'] = aid
+        # overwriting so the run uses the defaults
+        self.params['reference-db'] = ('/Users/jonsanders/Development/'
+                                       'anaconda/envs/qiita_env/lib/python2.7/'
+                                       'site-packages/kneaddata/tests/'
+                                       'data/demo_bowtie2_db/demo_db')
+
+        data = {'user': 'demo@microbio.me',
+                'command': dumps(['KneadData', '0.5.1', 'KneadData']),
+                'status': 'running',
+                'parameters': dumps(self.params)}
+        jid = self.qclient.post('/apitest/processing_job/', data=data)['job']
+
+        out_dir = mkdtemp()
+        self._clean_up_files.append(out_dir)
+
+        success, ainfo, msg = kneaddata(self.qclient, jid,
+                                        self.params, out_dir)
+
+        self.assertEqual("", msg)
+        self.assertTrue(success)
+        # we are expecting 16 artifacts per sample eventually
+        # but for now just the four fastqs
+        self.assertEqual(4, len(ainfo))
+
+        obs_fps = []
+        for a in ainfo:
+            self.assertEqual('per_sample_FASTQ', a.artifact_type)
+            obs_fps.append(a.files)
+        exp_fps = [[(join(out_dir, 'genefamilies.biom'), 'biom')],
+                   [(join(out_dir, 'pathcoverage.biom'), 'biom')],
+                   [(join(out_dir, 'pathabundance.biom'), 'biom')],
+                   [(join(out_dir, 'genefamilies_cpm.biom'), 'biom')]]
+        self.assertItemsEqual(exp_fps, obs_fps)
+
 
 
 MAPPING_FILE = (
