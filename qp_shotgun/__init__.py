7c6104b41c4bb4f503d08e95a4e8d719b9be2df4
Jose Navas <josenavasmolina@gmail.com>
2016-10-26 06:57:01 -0700

    Fixing imports

--- qp_shotgun/__init__.py
+++ qp_shotgun/__init__.py
@@ -5,3 +5,14 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # -----------------------------------------------------------------------------
+
+from qiita_client import QiitaPlugin
+
+from .humann2 import humann2_cmd
+
+# Initialize the plugin
+plugin = QiitaPlugin(
+    'HUMAnN2', '0.9.1', 'HUMAnN2 is the next generation of HUMAnN (HMP '
+    'Unified Metabolic Analysis Network)')
+
+plugin.register_command(humann2_cmd)
--- qp_shotgun/humann2/__init__.py
+++ qp_shotgun/humann2/__init__.py
@@ -6,17 +6,11 @@
 # The full license is in the file LICENSE, distributed with this software.
 # -----------------------------------------------------------------------------
 
-from qiita_client import QiitaPlugin, QiitaCommand
+from qiita_client import QiitaCommand
 
 from .humann2 import humann2
 
 __all__ = ['humann2']
-
-
-# Initialize the plugin
-plugin = QiitaPlugin(
-    'HUMAnN2', '0.9.1', 'HUMAnN2 is the next generation of HUMAnN (HMP '
-    'Unified Metabolic Analysis Network)')
 
 # Define the HUMAnN2 command
 req_params = {'input': ('artifact', ['per_sample_FASTQ'])}
@@ -97,4 +91,3 @@ dflt_param_set = {
 humann2_cmd = QiitaCommand(
     "HUMAnN2", "Community profiling", humann2, req_params, opt_params,
     outputs, dflt_param_set)
-plugin.register_command(humann2_cmd)
