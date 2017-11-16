9504d7701ac77ab0ef7e926b03ed654a91e12328
Jon Sanders <jsanders@oeb.harvard.edu>
2017-11-16 11:45:07 -0800

    Updated with Atropos defaults

--- qp_shotgun/qc-trim/__init__.py
+++ qp_shotgun/qc-trim/__init__.py
@@ -8,66 +8,33 @@
 
 from qiita_client import QiitaCommand
 
-from .kneaddata import kneaddata
+from .qc_trim import qc_trim
 
-__all__ = ['kneaddata']
+__all__ = ['qc_trim']
 
 # Define the kneaddata command
 req_params = {'input': ('artifact', ['per_sample_FASTQ'])}
 opt_params = {
-    # there are other parameters not included that will be ignored in this
-    # configuration as we assume that the correct values were set in the env
-    # by the admin installing the tools:
-    # trimmomatic
-    # bowtie2
-    # --input # input FASTQ file (add a second argument for paired input files)
-    # --output # directory to write output files
-    # --output-prefix # prefix for output files [ DEFAULT : $SAMPLE_kneaddata ]
-    # --log # filepath for log [ DEFAULT : $OUTPUT_DIR/$SAMPLE_kneaddata.log ]
-    # --bowtie2 # path to bowtie executable
-    # --bmtagger # path to bmtagger exectuable
-    # --trf # path to TRF executable
-    # --store-temp-output # store temp output files
-    # --trimmomatic # the location of the trimmomatic (automatically
-    #               # installed by pip install kneaddata)
-    # --log-level
-    'reference-db': ['choice:["default"]', 'default'],  # ref db
-    'bypass-trim': ['boolean', 'False'],  # bypass the trim step
-    'threads': ['integer', '1'],  # threads to run
-    'processes': ['integer', '1'],  # processes to run
-    'quality-scores': ['choice:["phred33", "phred64"]', 'phred33'],  # quality
-    'run-bmtagger': ['boolean', 'False'],  # run BMTagger instead of Bowtie2
-    'run-trf': ['boolean', 'False'],  # run TRF repeat finder tool
-    'run-fastqc-start': ['boolean', 'True'],  # run FastQC on original data
-    'run-fastqc-end': ['boolean', 'False'],  # run FastQC on filtered data
-    # Trimmomatic options
-    'max-memory': ['string', '500m'],  # max memory in mb [ DEFAULT : 500m ]
-    # leaving as empty string to simply. note that in tests is not empty.
-    'trimmomatic-options': ['string', ''],
-
-    # Bowtie2 options
-    # 'bowtie2-options': ['string', '--very-sensitive']
-    # BMTagger options
-    # # TRF options
-    # 'match': ['integer', '2'], # matching weight
-    # 'mismatch': ['integer', '7'], # mismatching penalty
-    # 'delta': ['integer', '7'], # indel penalty
-    # 'pm': ['integer', '80'], # match probability
-    # 'pi': ['integer', '10'], # indel probability
-    # 'minscore': ['integer', '50'], # mimimum alignment score to report
-    # 'maxperiod': ['integer', '500m'] # maximum period size to report
-    # FastQC options
+    'adapter': ['string', 'GATCGGAAGAGCACACGTCTGAACTCCAGTCAC'], # 3' adapter
+    'A': ['string', 'GATCGGAAGAGCGTCGTGTAGGGAAAGGAGTGT'], # 3' adapter for rev
+    'quality-cutoff': ['integer', '15'], # 3' quality cutoff
+    'minimum-length': ['integer', '80'], # min length after trimming 
+    'pair-filter': ['choice:["any", "both"]', 'any'], # drop pairs whose mates
+    # are filtered out
+    'max-n': ['integer', '80'], # maximum Ns to drop sequence
+    'trim-n': ['boolean', 'True'], # trim Ns on end of read
+    'nextseq-trim': ['boolean', 'False'], # NextSeq-specific quality trimming
     }
-outputs = {'KneadData files': 'per_sample_FASTQ'}
+outputs = {'Adapter trimmed files': 'per_sample_FASTQ'}
 dflt_param_set = {
-    'Defaults': {
-        'reference-db': 'default', 'bypass-trim': False, 'threads': 1,
-        'processes': 1, 'quality-scores': 'phred33', 'run-bmtagger': False,
-        'run-trf': False, 'run-fastqc-start': True, 'run-fastqc-end': False,
-        'max-memory': '500m', 'trimmomatic-options': ''
+    'KAPA HyperPlus with iTru': {
+        'adapter': 'GATCGGAAGAGCACACGTCTGAACTCCAGTCAC',
+        'A': 'GATCGGAAGAGCGTCGTGTAGGGAAAGGAGTGT', 
+        'quality-cutoff': 15, 'minimum-length': 80, 'pair-filter': 'any',
+        'max-n': 80, 'trim-n': True, 'nextseq-trim': False
         }
 }
 
-kneaddata_cmd = QiitaCommand(
-    'KneadData 0.5.1', "Sequence QC - Human sequence removal", kneaddata,
+qc_trim_cmd = QiitaCommand(
+    'Atropos v1.1.5', "Sequence QC - adapter trimming", qc_trim,
     req_params, opt_params, outputs, dflt_param_set)
