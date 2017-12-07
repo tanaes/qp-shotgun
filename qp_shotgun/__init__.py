039449e9d159300e036a628b47a7e9711af7674c
root <root@DESKTOP-DC3MPC5.localdomain>
2017-12-07 09:45:50 -0800

    removing humann2

--- qp_shotgun/__init__.py
+++ qp_shotgun/__init__.py
@@ -8,12 +8,10 @@
 
 from qiita_client import QiitaPlugin
 
-from .humann2 import humann2_cmd
 from .qc_trim import qc_trim_cmd
 
 # Initialize the plugin
 plugin = QiitaPlugin(
-    'qp-shotgun', '0.0.2', 'Analysis tools for shotgun data')
+    'qp-shotgun', '0.0.1', 'Analysis tools for shotgun data')
 
-plugin.register_command(humann2_cmd)
 plugin.register_command(qc_trim_cmd)
