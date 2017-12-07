739219478d43eb9905260f0e1400f16bd9aa61c2
root <root@DESKTOP-DC3MPC5.localdomain>
2017-12-07 09:43:13 -0800

    removing kneaddata

--- qp_shotgun/__init__.py
+++ qp_shotgun/__init__.py
@@ -9,7 +9,6 @@
 from qiita_client import QiitaPlugin
 
 from .humann2 import humann2_cmd
-#from .kneaddata import kneaddata_cmd
 from .qc_trim import qc_trim_cmd
 
 # Initialize the plugin
@@ -17,5 +16,4 @@ plugin = QiitaPlugin(
     'qp-shotgun', '0.0.2', 'Analysis tools for shotgun data')
 
 plugin.register_command(humann2_cmd)
-#plugin.register_command(kneaddata_cmd)
 plugin.register_command(qc_trim_cmd)
