37b625531a7b781b1fabf64f0cc3528860f67cea
root <root@DESKTOP-DC3MPC5.localdomain>
2018-01-11 04:29:59 -0800

    Removed redundant function

--- qp_shotgun/qc_filter/qc_filter.py
+++ qp_shotgun/qc_filter/qc_filter.py
@@ -12,6 +12,7 @@ from os.path import basename, join, exists
 from functools import partial
 from tempfile import mkdtemp
 from shutil import rmtree
+from qp_shotgun.qc_trim.qc_trim import make_read_pairs_per_sample
 
 from qiita_client import ArtifactInfo
 
@@ -23,99 +24,6 @@ BOWTIE2_PARAMS = {
 
 DATABASE_PREFIX = {
     'Human': 'Human/phix'}
-
-
-def make_read_pairs_per_sample(forward_seqs, reverse_seqs, map_file):
-    """Recovers read pairing information
-
-    Parameters
-    ----------
-    forward_seqs : list of str
-        The list of forward seqs filepaths
-    reverse_seqs : list of str
-        The list of reverse seqs filepaths
-    map_file : str
-        The path to the mapping file
-
-    Returns
-    -------
-    samples: list of tup
-        list of 4-tuples with run prefix, sample name, fwd read fp, rev read fp
-
-    Raises
-    ------
-    ValueError
-        If the rev is not an empty list and the same length than fwd seqs
-        The prefixes of the run_prefix don't match the file names
-
-    Notes
-    -----
-    At this stage it is required that if reverse sequences are present that all
-    samples have both a forward and a reverse sequence.
-    """
-
-    # sort forward seqs
-    forward_seqs.sort()
-
-    # check that rev seqs are same len
-    if reverse_seqs:
-        if len(forward_seqs) != len(reverse_seqs):
-            raise ValueError('Your reverse and forward files are of different '
-                             'length. Forward: %s. Reverse: %s.' %
-                             (', '.join(forward_seqs),
-                              ', '.join(reverse_seqs)))
-        reverse_seqs.sort()
-
-    # get run prefixes
-    # These are prefixes that should match uniquely to forward reads
-    # sn_by_rp is dict of samples keyed by run prefixes
-    sn_by_rp = get_sample_names_by_run_prefix(map_file)
-
-    # make pairings
-    samples = []
-    used_prefixes = set()
-    for i, (fwd_fp, rev_fp) in enumerate(zip_longest(forward_seqs,
-                                                     reverse_seqs)):
-        # fwd_fp is the fwd read filepath
-        fwd_fn = basename(fwd_fp)
-
-        # iterate over run prefixes and make sure only one matches
-        run_prefix = None
-        for rp in sn_by_rp:
-            if fwd_fn.startswith(rp) and run_prefix is None:
-                run_prefix = rp
-            elif fwd_fn.startswith(rp) and run_prefix is not None:
-                raise ValueError('Multiple run prefixes match this fwd read: '
-                                 '%s' % fwd_fn)
-
-        # make sure that we got one matching run prefix:
-        if run_prefix is None:
-            raise ValueError('No run prefix matching this fwd read: %s'
-                             % fwd_fn)
-
-        if run_prefix in used_prefixes:
-            raise ValueError('This run prefix matches multiple fwd reads: '
-                             '%s' % run_prefix)
-
-        if rev_fp is None:
-            samples.append((run_prefix, sn_by_rp[run_prefix], fwd_fp, None))
-        else:
-            rev_fn = basename(rev_fp)
-            # if we have reverse reads, make sure the matching pair also
-            # matches the run prefix:
-            if not rev_fn.startswith(run_prefix):
-                raise ValueError('Reverse read does not match this run prefix.'
-                                 '\nRun prefix: %s\nForward read: %s\n'
-                                 'Reverse read: %s\n' %
-                                 (run_prefix, fwd_fn, rev_fn))
-
-            samples.append((run_prefix, sn_by_rp[run_prefix], fwd_fp,
-                            rev_fp))
-
-        used_prefixes.add(run_prefix)
-
-    return(samples)
-
 
 def _format_qc_filter_params(parameters):
     params = []
